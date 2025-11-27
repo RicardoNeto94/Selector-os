f72c357f1bc646a1f2aaedbbb07c5332a118210a
RicardoNeto94 <49795011+RicardoNeto94@users.noreply.github.com>
2025-11-27 21:40:41 +0200

    Update page.js

--- src/app/r/[slug]/allergens/page.js
+++ src/app/r/[slug]/allergens/page.js
@@ -1,1 +1,9 @@
+export default function AllergenList() {
+  return (
+    <div className="card">
+      <h1 className="text-2xl font-bold mb-4">Allergens</h1>
+      <p>Allergens will be displayed here.</p>
+    </div>
+  );
+}
 
