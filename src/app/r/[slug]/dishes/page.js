23bd3aa8bd36290ce712bc86ff15c429e50fbcc3
RicardoNeto94 <49795011+RicardoNeto94@users.noreply.github.com>
2025-11-27 21:40:23 +0200

    Update page.js

--- src/app/r/[slug]/dishes/page.js
+++ src/app/r/[slug]/dishes/page.js
@@ -1,1 +1,9 @@
+export default function DishList() {
+  return (
+    <div className="card">
+      <h1 className="text-2xl font-bold mb-4">Dishes</h1>
+      <p>Dishes will be listed here.</p>
+    </div>
+  );
+}
 
