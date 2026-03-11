9e42babe272f464febf7f20be469a9f1eb50edf3
RicardoNeto94 <49795011+RicardoNeto94@users.noreply.github.com>
2026-03-11 18:40:04 +0200

    Update layout.js

--- src/app/r/[slug]/layout.js
+++ src/app/r/[slug]/layout.js
@@ -1,7 +1,16 @@
+import "../../../styles/guest.css";
+
 export default function PublicLayout({ children }) {
   return (
     <html>
-      <body style={{ margin: 0, padding: 0 }}>
+      <body
+        style={{
+          margin: 0,
+          padding: 0,
+          background: "#020617",
+          color: "white"
+        }}
+      >
         {children}
       </body>
     </html>
