9f9a74fad179ca1ba6cfce91e2fa285f29baaa4d
RicardoNeto94 <49795011+RicardoNeto94@users.noreply.github.com>
2026-03-11 23:54:07 +0200

    Update page.js

--- src/app/dashboard/wines/new/page.js
+++ src/app/dashboard/wines/new/page.js
@@ -99,8 +99,8 @@ export default function NewWinePage() {
 
         <form
   onSubmit={handleSubmit}
-  className="so-card w-full p-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
-        >
+  className="so-card w-full max-w-6xl p-10 !grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
+>
 
           <input
             className="so-input"
