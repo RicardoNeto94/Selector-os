6cb16410b689debed2f3ce80b3b1348505e16845
RicardoNeto94 <49795011+RicardoNeto94@users.noreply.github.com>
2026-03-15 10:58:23 +0200

    Reduce top spacing on wine selector hero section

--- src/app/wine/[restaurant]/[menu]/page.js
+++ src/app/wine/[restaurant]/[menu]/page.js
@@ -184,7 +184,7 @@ Wine Selection
 
 )}
 
-<div className="min-h-screen bg-[#f8f6f1] text-[#1c1c1c] flex flex-col items-center px-6 pt-28 pb-20">
+<div className="min-h-screen bg-[#f8f6f1] text-[#1c1c1c] flex flex-col items-center px-6 pt-12 pb-20">
 
 <div className="flex flex-col items-center text-center mb-12">
 
