06bace080e53624ede88ff2b28a1da7b8adbd750
RicardoNeto94 <49795011+RicardoNeto94@users.noreply.github.com>
2026-04-26 02:22:09 +0300

    Add domain routing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "burman.vaxeron.com",
+          },
+        ],
+        destination: "/menu/burman-hotel",
+      },
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "foxden.vaxeron.com",
+          },
+        ],
+        destination: "/menu/foxden",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
