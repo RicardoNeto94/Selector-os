c5626a292f39c0ba5754b5f662982e36533f10aa
RicardoNeto94 <49795011+RicardoNeto94@users.noreply.github.com>
2026-04-27 21:25:20 +0300

    viewport

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,12 +34,11 @@ export default function RootLayout({ children }) {
       <head>
         {/* PWA */}
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#2a0000" />
 
         {/* iOS FIX (🔥 important) */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="SelectorOS" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
 
         <link rel="apple-touch-icon" href="/icon-192.png" />
 
