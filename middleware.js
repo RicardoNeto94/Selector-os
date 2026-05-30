eba2d09e3dfee874c141844133dbacd874a12f6f
RicardoNeto94 <49795011+RicardoNeto94@users.noreply.github.com>
2026-05-30 19:11:21 +0300

    Add spa koyo shangshi subdomains

--- middleware.js
+++ middleware.js
@@ -1,15 +1,33 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export function middleware(req) {
+
   const host = req.headers.get("host") || "";
   const { pathname } = req.nextUrl;
 
-  // ✅ ONLY rewrite root "/"
   if (pathname === "/") {
 
     if (host.includes("burman.vaxeron.com")) {
       return NextResponse.rewrite(
         new URL("/menu/burman-hotel", req.url)
+      );
+    }
+
+    if (host.includes("spa.vaxeron.com")) {
+      return NextResponse.rewrite(
+        new URL("/spa/burman", req.url)
+      );
+    }
+
+    if (host.includes("koyo.vaxeron.com")) {
+      return NextResponse.rewrite(
+        new URL("/wine/koyo-wine", req.url)
+      );
+    }
+
+    if (host.includes("shangshi.vaxeron.com")) {
+      return NextResponse.rewrite(
+        new URL("/wine/shang-shi-wine", req.url)
       );
     }
 
@@ -22,8 +40,9 @@ export function middleware(req) {
   }
 
   return NextResponse.next();
+
 }
 
 export const config = {
-  matcher: ['/'],
+  matcher: ["/"],
 };
